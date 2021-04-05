18c8ec45ed16fbf26ea8a846d597772045cdd4f8
eun <cseongeun94@gmail.com>
2021-04-05 16:24:37 +0900

    func util

--- utils/func.ts
+++ utils/func.ts
@@ -10,29 +10,13 @@ export function randomBytes(len: number): string {
   return utils.hexlify(utils.randomBytes(len));
 }
 
-// export function pickStruct(struct: any): object {
-//   return Object.keys(struct).reduce((acc: any, key: any) => {
-//     if (Number.isNaN(Number(key))) {
-//       let value = struct[key];
-//       return {
-//         ...acc,
-//         [key]: struct[key] === BigNumber.isBigNumber(value) ? BigNumber.from(value) : value,
-//       };
-//     } else {
-//       return acc;
-//     }
-//   }, {});
-// }
-
-
 export function formatStruct(struct: Object): object {  
   const res: any = {};
   for (const [key, value] of Object.entries(struct)) {
     if (!isNaN(parseInt(key))) {
       continue;
     } 
-    if (BigNumber.isBigNumber(value)) res[key] = BigNumber.from(value)
-    else res[`${key}`] = value;
+    res[key] = BigNumber.isBigNumber(value) ? BigNumber.from(value) : value; 
   }
   return res;
 }
